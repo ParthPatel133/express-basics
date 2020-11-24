93951a3efd81dc8f7c8d2b08d7becb0e7767d2d7
parthpatel133 <prpatel133@gmail.com>
2020-11-24 18:06:03 +0530

    introducing pug with express

--- express/app.js
+++ express/app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 80;
+
+//for serving static files
+app.use('/static', express.static('static')) //here static is folder name
+
+//set the template engine as pug
+app.set('view engine', 'pug')
+
+//set the views directory
+app.set('views', path.join(__dirname, 'views'))
+
+//pug demo end point
+app.get('/demo', (req, res) => {
+    res.status(200).render('demo', { title: 'pug demo', message: 'sample text for using pug' })
+})
 
 app.get("/", (req, res) => {
     res.status(200).send('this is homepage')
 })
+
 
 app.post("/about", (req, res) => {
     res.status(200).send('this is about post request')
